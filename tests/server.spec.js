a1a530c85d24549db81783a88b12b320f900b017
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-03-04 23:53:29 +0600

    Add tests to get 404 when request unknown page

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -5,9 +5,15 @@ describe('basic', function () {
     this.server = require('../server/server');
   });
 
-  it('/', done => {
+  it('should have / page', done => {
     request(this.server)
       .get('/')
       .expect(200, done);
+  });
+
+  it('should send 404 if type to get unknown page', done => {
+    request(this.server)
+      .get('/not-found')
+      .expect(404, done);
   });
 });
